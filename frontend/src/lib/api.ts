87a7d8cf14d78e89906142f2b4a7e584d6ca498b
IshantDeoghare <deoghareishant@gmail.com>
2025-07-13 14:05:28 +0530

    Update api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001/api';
+const API_BASE_URL ="https://pro-book-marketplace-api.onrender.com/api" //|| 'http://localhost:5001/api';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
